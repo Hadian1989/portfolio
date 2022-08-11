2e393956d740965e011da8e07d8f50a0cb2c3899
Ghazal1989 <ghazal.hadiyan@gmail.com>
2022-08-11 17:39:23 +0430

    create several components in front app

--- front/portfolio/src/app/app.module.ts
+++ front/portfolio/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AboutComponent } from './about/about.component';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
+import { BaseComponent } from './base/base.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    ContactComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    BaseComponent,
+    ProjectsComponent,
+    ProjectDetailsComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
